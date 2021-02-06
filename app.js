3ab7eaba241ec18ce74a52fb14fffe42abf18e38
gunish.jha <gunish.jha.master@Gmail.com>
2021-02-06 23:50:46 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+// console.log("Script is working from a diffrent console")
+
+//input
+var username = prompt("Give me username")
+// processing
+var welcomeMessage = "This script works !!" + username
+//output
+alert(welcomeMessage)
