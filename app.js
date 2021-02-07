eedf896b3ce9bf174839e32b86f998a1580e72c9
gunish.jha <gunish.jha.master@Gmail.com>
2021-02-07 11:35:56 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input") 
+var outputDiv = document.querySelector("#output");
+outputDiv.innerText = "Gunish Jha"
+
+console.log(outputDiv)
 
 
 function clickEventHandler() {     
-    console.log("clcked");
-    console.log("input",txtInput.value)
+    outputDiv.innerText= "Appended text is here " + txtInput.value; 
 
 };
 
 btnTranslate.addEventListener("click", clickEventHandler)
 
-// 1. document.querySelector ("textarea")
-// 2. .btn-primary
-// 3. #input-btn
-// 4. "input[name='translator']"
 
-// document.querySelectorAll(".artdeco-button__text").forEach(item=> setTimeout(item.click(), Math.random*1000))
 
 
 
